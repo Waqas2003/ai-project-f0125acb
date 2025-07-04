21ae8a11b5bcced6fc75ac758e217af725260c10
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 00:46:20 +0500

    Add src\components\About.js

---
+++ src\components\About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="about">
+      <h2>About Us</h2>
+      <p>The Pakistan Cricket Team, also known as the Men in Green, represents Pakistan in international cricket.</p>
+      <p>The team has a rich history and has produced some of the greatest cricketers of all time.</p>
+    </section>
+  );
+}
+
+export default About;
