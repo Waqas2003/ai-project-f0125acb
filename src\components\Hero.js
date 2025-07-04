27990b74cb7712ef634f05a60b2cbac6d875b29c
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 00:46:25 +0500

    Add src\components\Hero.js

---
+++ src\components\Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Pakistan Cricket Team</h1>
+      <p>The official website of the Pakistan Cricket Team</p>
+      <button>Learn More</button>
+    </section>
+  );
+}
+
+export default Hero;
